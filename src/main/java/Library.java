079421a764dd1e15168129f174c58901841c0a57
evelyn-sz <evelynszarek@gmail.com>
2021-02-09 22:25:57 +0000

    Refactored code so library checks capacity before adding a book

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class Library {
     private String name;
+    private int capacity;
     private ArrayList<Book> bookList;
 
-    public Library(String name){
+    public Library(String name, int capacity){
         this.name = name;
+        this.capacity = capacity;
         this.bookList = new ArrayList<Book>();
     }
     public String getName(){
@@ -21,6 +23,7 @@ public class Library {
     }
 
     public void addBook(Book book) {
+        if (this.countBooks() < this.capacity)
         this.bookList.add(book);
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,13 +8,15 @@ public class LibraryTest {
     private Book lotr;
     private Book hp;
     private Book ocean;
+    private Book geo;
 
     @Before
     public void before(){
-        wordFactory = new Library("Word Factory");
+        wordFactory = new Library("Word Factory", 3);
         lotr = new Book("LOTR", "Tolkien", "Fantasy");
         hp = new Book("Harry Potter", "Rowling", "Fantasy");
         ocean = new Book("Oceanography", "Thurman", "Science");
+        geo = new Book("Geochemistry", "Killops", "Science");
         wordFactory.addBook(lotr);
         wordFactory.addBook(hp);
         wordFactory.addBook(ocean);
@@ -27,6 +29,12 @@ public class LibraryTest {
     }
     @Test
     public void canCountNumberOfBooks(){
+        assertEquals(3, wordFactory.countBooks());
+    }
+
+    @Test
+    public void cannotGoOverCapacity(){
+        wordFactory.addBook(geo);
         assertEquals(3, wordFactory.countBooks());
     }
 
